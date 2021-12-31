5132edfc5bbf7c7bf211ec13f34315030a56470d
thuan <thuan.hoang@vastbit.com>
2021-12-31 11:00:24 +0700

    a

---
+++ ios/RCTTvnSdk/TvnSdk.h
@@ -0,0 +1,14 @@
+//
+//  TvnSdk.h
+//  TvnSdk
+//
+//  Created by Admin on 31/12/2021.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#ifndef TvnSdk_h
+#define TvnSdk_h
+#import "TiUIManager.h"
+#import <TiSDK/TiSDKInterface.h>
+
+#endif /* TvnSdk_h */
